759fbc08ea128ec4fdff2fa3153511fd0607beec
dmitriy.b <dmitriy.b@smplt.net>
2022-03-15 17:33:17 +0500

    Add migration

---
+++ src/Service.PaymentDepositRepository.Postgres/Migrations/20220315123202_RemoveCardFields.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.PaymentDepositRepository.Postgres.Migrations
+{
+    public partial class RemoveCardFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CardCvv",
+                schema: "education",
+                table: "payment_deposit");
+
+            migrationBuilder.DropColumn(
+                name: "CardHolder",
+                schema: "education",
+                table: "payment_deposit");
+
+            migrationBuilder.DropColumn(
+                name: "CardMonth",
+                schema: "education",
+                table: "payment_deposit");
+
+            migrationBuilder.RenameColumn(
+                name: "CardYear",
+                schema: "education",
+                table: "payment_deposit",
+                newName: "CardNumberName");
+
+            migrationBuilder.RenameColumn(
+                name: "CardNumber",
+                schema: "education",
+                table: "payment_deposit",
+                newName: "CardNumberHash");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CardNumberName",
+                schema: "education",
+                table: "payment_deposit",
+                newName: "CardYear");
+
+            migrationBuilder.RenameColumn(
+                name: "CardNumberHash",
+                schema: "education",
+                table: "payment_deposit",
+                newName: "CardNumber");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CardCvv",
+                schema: "education",
+                table: "payment_deposit",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CardHolder",
+                schema: "education",
+                table: "payment_deposit",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CardMonth",
+                schema: "education",
+                table: "payment_deposit",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
